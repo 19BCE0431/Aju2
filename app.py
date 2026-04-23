2d75e6bfeeb8da5d63100bbe61f8344e00b95459
Mohit sai krishna peddakotla <67175641+19BCE0431@users.noreply.github.com>
2026-04-23 09:43:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
+
 import streamlit as st
 import requests
 import pandas as pd
-
-# sd
 
 API_URL = "https://aju-production.up.railway.app"
 
 st.title("📊 Financial PDF Smart Search")
 
-# ---------------------------
-# UPLOAD
-# ---------------------------
 file = st.file_uploader("Upload your PDF")
+# files = {"file": (file.name, file.getvalue(), "application/pdf")}
 
 if file:
     files = {"file": file.getvalue()}
-
+    files = {"file": (file.name, file.getvalue(), "application/pdf")}
     res = requests.post(f"{API_URL}/upload", files=files)
 
     if res.status_code == 200:
@@ -23,10 +20,6 @@ if file:
     else:
         st.error("❌ Upload failed")
 
-
-# ---------------------------
-# SEARCH
-# ---------------------------
 query = st.text_input("🔍 Search (example: chethana)")
 
 if query:
@@ -34,15 +27,13 @@ if query:
 
     if res.status_code == 200:
         response = res.json()
-
         data = response.get("results", [])
 
-        if len(data) == 0:
+        if not data:
             st.warning("No results found")
         else:
             df = pd.DataFrame(data)
 
-            # Ensure columns exist safely
             for col in ["debit", "credit", "balance"]:
                 if col in df.columns:
                     df[col] = pd.to_numeric(df[col], errors="coerce").fillna(0)
